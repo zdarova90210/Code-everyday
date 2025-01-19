3c7f3e8f2eeacb5d3a903abeee4e63627fab64d5
zdarova90210 <zdarova90210@gmail.com>
2025-01-19 20:15:40 +0500

    Add more tests for palindrome task

--- tasks/palindrome/index.test.js
+++ tasks/palindrome/index.test.js
@@ -1,13 +1,22 @@
 const isPalindrome = require('./index');
+describe('Palindrome', () => {
+    it('racecar is a palindrome', () => {
+        expect(isPalindrome('racecar')).toBe(true);
+    });
 
-test('racecar is a palindrome', () => {
-    expect(isPalindrome('racecar')).toBe(true);
-});
+    it('RacECaR is a palindrome', () => {
+        expect(isPalindrome('RacECaR')).toBe(true);
+    });
 
-test('hello is not a palindrome', () => {
-    expect(isPalindrome('hello')).toBe(false);
-});
+    it('RacECaR1 is not a palindrome', () => {
+        expect(isPalindrome('RacECaR1')).toBe(false);
+    });
 
-test('A man a plan a canal Panama', () => {
-    expect(isPalindrome('A man a plan a canal Panama')).toBe(true);
-});
+    it('hello is not a palindrome', () => {
+        expect(isPalindrome('hello')).toBe(false);
+    });
+
+    it('A man a plan a canal Panama', () => {
+        expect(isPalindrome('A man a plan a canal Panama')).toBe(true);
+    });
+})
