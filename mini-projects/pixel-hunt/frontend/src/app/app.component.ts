948b0b423217f43233d69415877f3a90f5222cf1
zdarova90210 <zdarova90210@gmail.com>
2025-01-25 11:31:19 +0500

    Add test image, add base README.md

--- mini-projects/pixel-hunt/frontend/src/app/app.component.ts
+++ mini-projects/pixel-hunt/frontend/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'pixel-hunt';
+  form: FormGroup;
+
+  constructor(private _fb: FormBuilder) {
+    this.form = this._fb.group({
+      answer: ['', Validators.required]
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value.answer);
+    this.form.reset();
+  }
 }
