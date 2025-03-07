e7196fc95e02c0ec7624b94a39961e7a89b9c057
zdarova90210 <zdarova90210@gmail.com>
2025-03-07 20:00:02 +0500

    Play with signal.set(), signal.update()

--- ngSandbox/src/app/topics/signals-demo/signals-demo.component.ts
+++ ngSandbox/src/app/topics/signals-demo/signals-demo.component.ts
@@ -2,19 +2,26 @@ import {ChangeDetectionStrategy, Component, computed, signal} from '@angular/cor
 
 @Component({
   template: `
-    <button (click)="increment()">Print signals</button>
+    <button (click)="setTo1()">Set to 1</button>
+    <button (click)="incrementBy1()">Increment by 1</button>
+    <br> <br>
+    Counter: {{ counter() }}
+    <br> <br>
+    Computed: {{ computedCount() }}
   `,
   standalone: true,
   imports: [],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SignalsDemoComponent {
-  public count = signal(0);
-  public computedCount = computed(() => this.count() * 10);
+  public counter = signal(0);
+  public computedCount = computed(() => this.counter() * 10);
 
-  public increment() {
-    this.count.set(this.count() + 1);
-    console.log(this.count());
-    console.log(this.computedCount());
+  public setTo1() {
+    this.counter.set(1);
+  }
+
+  public incrementBy1(): void {
+    this.counter.update(v => v + 1);
   }
 }
