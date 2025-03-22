8a5fb0b4f05da1b76b5aba5edfeb77d0f98431ea
zdarova90210 <zdarova90210@gmail.com>
2025-03-23 01:34:30 +0500

    Refactor card component to add 3D flip effect and grid layout

--- mini-projects/memory-cards/src/app/app.component.ts
+++ mini-projects/memory-cards/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
 import {CardComponent} from './compponents/card/card.component';
-import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +8,5 @@ import {NgForOf} from '@angular/common';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+  values = ['😍', '💖', '😎', '😻', '🥸', '🐈', '🐈', '😍', '💖', '😻', '😎', '🥸'];
 }
