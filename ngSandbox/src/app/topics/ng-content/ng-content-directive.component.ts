cd2d5a35ba94227ce8213170a1ee9b03cc52c09b
zdarova90210 <zdarova90210@gmail.com>
2025-05-07 18:06:10 +0500

    Add NgContent demo with directive and select usage examples
    Implemented `NgContentDirectiveComponent` and `NgContentPageComponent` to demonstrate the use of `ng-content` with fallback content and select keyword. Included styling, templates, and routing updates to integrate the new components into the application.

---
+++ ngSandbox/src/app/topics/ng-content/ng-content-directive.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-ng-content-directive',
+  templateUrl: './ng-content-directive.component.html',
+  styleUrls: ['./ng-content-directive.component.scss'],
+  standalone: true
+})
+export class NgContentDirectiveComponent {
+
+}
